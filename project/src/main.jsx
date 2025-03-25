cf44bd21bf3485f60b4cbef7d625871291129113
AvaniK <avkulkrni2002@gmail.com>
2025-03-26 00:07:06 +0530

    Fix GitHub Pages routing issue

--- project/src/main.jsx
+++ project/src/main.jsx
@@ -1,12 +1,12 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App.jsx';
-import './index.css';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename="/Portfolio-React">  {/* ✅ Add basename */}
+    <BrowserRouter basename="/Portfolio-React">
       <App />
     </BrowserRouter>
   </StrictMode>
